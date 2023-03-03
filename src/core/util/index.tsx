abde3b7c3211ebf98f30ab6d457bd65ff61e785f
Bruno VU <bruno.vu.ext@franctvpub.fr>
2023-03-04 02:43:56 +0700

    fix

--- src/core/util/index.tsx
+++ src/core/util/index.tsx
@@ -15,7 +15,7 @@ export const getQueryVariable = (arg) => {
     const query = window.location.search.substring(1);
     const vars = query.split("&");
 
-    for (var i=0; i<vars.length; i++) {
+    for (let i=0; i< vars.length; i++) {
         const pair = vars[i].split("=");
         if(pair[0] == arg){
             return pair[1];
